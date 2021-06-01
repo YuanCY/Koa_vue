d2e0adf976ab06ea7b70590f064cd63ef8b49ed5
samyuan <sam.yuancy@gmail.com>
2021-06-01 17:08:32 +0800

    加入后台页面基本布局，并完成header页面布局及设置，完成退出登陆按钮。

--- src/components/background/Background.vue
+++ src/components/background/Background.vue
@@ -1,13 +1,70 @@
 <template>
-  <div id="app">background</div>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <div class="title">博客后台管理</div>
+        <el-button round class="logoutBtn" size="small" @click="logoutDialogVisible = true">退出登陆</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <!-- ===============退出登陆弹窗============== -->
+          <el-dialog
+            title="提示"
+            :visible.sync="logoutDialogVisible"
+            width="30%"
+          >
+            <span>确定是否退出登陆？</span>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="logoutDialogVisible = false">取 消</el-button>
+              <el-button type="primary" @click="logout">确 定</el-button>
+            </span>
+          </el-dialog>
+          <!-- ===============退出登陆弹窗============== -->
+        Main
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      logoutDialogVisible: false // 退出显示弹窗
+    }
+  },
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+      this.logoutDialogVisible = false
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-header {
+  position: relative;
+  background-color: #3171ad;
+  line-height: 60px;
+}
+.title {
+  font-size: 30px;
+  color: #fff;
+}
+.el-aside {
+  background-color: #6aacde;
+}
+.el-main {
+  background-color: #eee;
+}
+.logoutBtn {
+  position: absolute;
+  top: 50%;
+  right: 1%;
+  transform: translateY(-50%);
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,12 +4,24 @@ import {
   Form,
   FormItem,
   Input,
-  Message
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Dialog
 } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Dialog)
 
 Vue.prototype.$message = Message
