a57d94fe16b175c359b8ec480f67b4486db48fd1
samyuan <sam.yuancy@gmail.com>
2021-07-06 15:31:48 +0800

    1、书写后端model，controller层，增删改查功能。

---
+++ server/controllers/category.js
@@ -0,0 +1,100 @@
+const categoryModel = require('../models/category')
+
+/**
+ * 查：获取get传入参数，pagenum页码，pagesize每页内容，query搜索参数，返回数据库中分类的信息
+ * @param {*} ctx
+ */
+async function getCategoryList(ctx) {
+  const data = ctx.query
+  const categoryList = await categoryModel.getAllCategory(parseInt(data.pagenum), parseInt(data.pagesize), data.query)
+  if (categoryList) {
+    ctx.body = {
+      success: true,
+      list: categoryList,
+      info: '获取列表成功'
+    }
+  } else {
+    ctx.body = {
+      success: false,
+      info: 'controllers：未获取到categoryList'
+    }
+  }
+}
+
+/**
+ * 增：通过获取post参数，在数据库中新增一个分类
+ * @param {*} ctx
+ */
+async function postAddCategory(ctx) {
+  const data = ctx.request.body // 获取ctx中post传入的参数
+  const categoryInfo = {
+    cagegoryName: data.cagegoryName
+  }
+  if (categoryInfo.cagegoryName === '') {
+    ctx.body = {
+      success: false,
+      info: '错误：分类名称为空！'
+    }
+  } else {
+    const addCategroyInfo = await categoryModel.addCategroyInfo(categoryInfo)
+    console.log(addCategroyInfo)
+    if (addCategroyInfo !== null) {
+      ctx.body = {
+        success: true,
+        info: '创建分类成功'
+      }
+    } else {
+      ctx.body = {
+        success: false,
+        info: 'model: 创建错误model层没给反馈'
+      }
+    }
+  }
+}
+
+/**
+ * 删：通过获取id，删除指定文章
+ * @param {*} ctx
+ */
+async function deleteCategory(ctx) {
+  const data = ctx.params // 获取到传入参数
+  const deleteInfo = await categoryModel.deleteCategory(data.id)
+  if (deleteInfo === 1) {
+    ctx.body = {
+      success: true,
+      info: '删除分类成功'
+    }
+  } else {
+    ctx.body = {
+      uccess: false,
+      nfo: 'model: 删除失败'
+    }
+  }
+}
+
+async function putEditCategory(ctx) {
+  const data = ctx.request.body
+  const categoryInfo = {
+    id: data.id,
+    cagegoryName: data.cagegoryName
+  }
+  const editCategoryInfo = await categoryModel.editCategory(categoryInfo)
+  if (editCategoryInfo[0] === 1) {
+    ctx.body = {
+      success: true,
+      info: '更新分类成功'
+    }
+  } else {
+    ctx.body = {
+      success: false,
+      info: 'model:更新失败'
+    }
+  }
+}
+
+module.exports = {
+  getCategoryList,
+  postAddCategory,
+  deleteCategory,
+  putEditCategory
+}
--- server/schema/category.js
+++ server/schema/category.js
@@ -1,30 +1,31 @@
-const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('category', {
-    id: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER.UNSIGNED,
-      allowNull: false,
-      primaryKey: true
-    },
-    cagegoryName: {
-      type: DataTypes.CHAR(50),
-      allowNull: false,
-      defaultValue: ""
-    }
-  }, {
-    sequelize,
-    tableName: 'category',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+  return sequelize.define(
+    'category',
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true
       },
-    ]
-  });
-};
+      cagegoryName: {
+        type: DataTypes.CHAR(50),
+        allowNull: false,
+        defaultValue: ''
+      }
+    },
+    {
+      sequelize,
+      tableName: 'category',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [{ name: 'id' }]
+        }
+      ]
+    }
+  )
+}
