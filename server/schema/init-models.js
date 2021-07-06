12cbb119238acda0041caa7c59a78b095ee5fda3
samyuan <sam.yuancy@gmail.com>
2021-07-06 15:01:00 +0800

    1、创建分类category的model层

--- server/schema/init-models.js
+++ server/schema/init-models.js
@@ -1,13 +1,16 @@
 var DataTypes = require('sequelize').DataTypes
 var _article = require('./article')
+var _category = require('./category')
 var _users = require('./users')
 
 function initModels(sequelize) {
   var article = _article(sequelize, DataTypes)
+  var category = _category(sequelize, DataTypes)
   var users = _users(sequelize, DataTypes)
 
   return {
     article,
+    category,
     users
   }
 }
